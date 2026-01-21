6bab4ce86295fc36aab91c45229223d9c339e57c
AlexandreMaia <empmaiaa@gmail.com>
2026-01-21 18:21:36 -0300

    atualizar pagina

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../services/Auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
