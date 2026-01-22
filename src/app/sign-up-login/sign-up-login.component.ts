dfafd8b28950f289abdf75b969e1ab785b519131
OlluaS-code <s.moura.tav@gmail.com>
2026-01-22 15:27:58 -0300

    Considerações finais

--- src/app/sign-up-login/sign-up-login.component.ts
+++ src/app/sign-up-login/sign-up-login.component.ts
@@ -2,26 +2,29 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/Auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './sign-up-login.component.html',
-  styleUrl: './sign-up-login.component.css'
+  styleUrl: './sign-up-login.component.css',
 })
 export class SignUpLoginComponent {
   userData = {
     email: '',
-    password: ''
+    password: '',
   };
 
   loading = false;
   errorMessage = '';
   successMessage = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
   async onRegister(event: Event) {
     event.preventDefault();
@@ -32,11 +35,10 @@ export class SignUpLoginComponent {
     try {
       await this.authService.register(this.userData);
       this.successMessage = 'Cadastro realizado com sucesso! Redirecionando...';
-      
+
       setTimeout(() => {
         this.router.navigate(['/login']);
       }, 2000);
-
     } catch (err: any) {
       this.errorMessage = err.message || 'Erro ao realizar cadastro.';
     } finally {
